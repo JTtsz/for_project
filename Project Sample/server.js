b28eb62b3c7c60946cc2357b18478b94fc526096
JTtsz <144902652+JTtsz@users.noreply.github.com>
2023-11-11 14:20:23 +0800

    Update server.js

--- Project Sample/server.js
+++ Project Sample/server.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 
-const mongourl = ''; 
+const mongourl = 'mongodb+srv://s1299841:1303313524Aa@cluster0.uuchtz5.mongodb.net/?retryWrites=true&w=majority'; 
 const dbName = 'test';
 
 const express = require('express');
